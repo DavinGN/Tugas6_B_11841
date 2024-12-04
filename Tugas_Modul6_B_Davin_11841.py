4371a2452fe7d78d8491c01f1fcaab3ebc8d4a84
DavinGN <220711841@students.uajy.ac.id>
2024-12-04 15:33:10 +0700

    Update Tugas_Modul6_B_Davin_11841.py

--- Tugas_Modul6_B_Davin_11841.py
+++ Tugas_Modul6_B_Davin_11841.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-model = load_model(r'D:\Sem 5\Pembelajaran Mesin\Tugas6_B_11841\model_mobilenet.h5')
+model = load_model(r'model_mobilenet.h5')
 class_names = ['Matang', 'Mentah']
 
 def classify_image(image_path):
